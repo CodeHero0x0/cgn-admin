7b8fc3d3457e100652ce2df842b5c7d659e3bc77
zyan <zyan888@88.com>
2025-08-29 15:50:09 +0800

    配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,47 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
-// 参考：https://cn.vitejs.dev/config/
-export default defineConfig({
-	base: '/',
-	resolve: {
-		// 配置别名
-		alias: {
-			'@': resolve(__dirname, './src'),
-			'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
-		}
-	},
-	plugins: [
-		vue(),
-		vueSetupExtend(),
-		createSvgIconsPlugin({
-			iconDirs: [resolve(__dirname, 'src/icons/svg')],
-			symbolId: 'icon-[dir]-[name]'
-		})
-	],
-	server: {
-		host: '0.0.0.0',
-		port: 3000, // 端口号
-		open: false // 是否自动打开浏览器
-	}
+// 将 defineConfig 的参数改为一个函数，以接收 mode 参数
+export default defineConfig(({mode}) => {
+    // 基于当前工作目录和模式加载 .env 文件
+    // process.cwd() 是当前项目根目录
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        base: '/',
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, './src'),
+                'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+            }
+        },
+        plugins: [
+            vue(),
+            vueSetupExtend(),
+            createSvgIconsPlugin({
+                iconDirs: [resolve(__dirname, 'src/icons/svg')],
+                symbolId: 'icon-[dir]-[name]'
+            })
+        ],
+        server: {
+            host: '0.0.0.0',
+            port: 3000,
+            open: false,
+
+            proxy: {
+                [env.VITE_API_BASE_URL]: {
+                    target: env.VITE_API_PROXY_TARGET,
+
+                    // 允许跨域
+                    changeOrigin: true,
+
+                    rewrite: path => path.replace(new RegExp('^' + env.VITE_API_BASE_URL), '')
+                }
+            }
+        }
+    }
 })
